9fa3a0f63038fb25dd33957d3dc00205934e15d1
Kirt Brainer Trinidad <kirttrinidad043@gmail.com>
2025-09-11 10:31:59 +0800

    Add files via upload

---
+++ trinidad_kirt.js
@@ -0,0 +1,39 @@
+const employees=[
+    {
+        name:"Alice",
+        id:1,
+        salary:45000,
+        position:'Developer'
+    },
+     {
+        name:"Bob",
+        id:2,
+        salary:60000,
+        position:'Manager'
+    },
+     {
+        name:"Charlie",
+        id:3,
+        salary:38000,
+        position:'Designer'
+    },
+     {
+        name:"Diana",
+        id:4,
+        salary:52000,
+        position:'Team Lead'
+    },
+     {
+        name:"Ethan",
+        id:5,
+        salary:47000,
+        position:'QA Engineer'
+    }
+]
+
+
+if(salary <= 50000){
+         console.log('The employee will receive a bonus of 5000')
+     }else (salary>=50000){
+         console.log('The employee will not receive a bonus')
+ }
